e120041fb81fb132e0aabdc89acbb884f4b298c4
jonn <jonnjonn@DESKTOP-DFCGIUM.localdomain>
2020-08-17 17:33:41 +0900

    Remove duplicate dialog and use a threshold for silence trimming

--- ExtractDialog.go
+++ ExtractDialog.go
@@ -8,61 +8,83 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"sort"
 	"strings"
+	"time"
 )
 
 const (
-	tempDir = "./tmp/"
+	tempDir = "./tmp_new/"
+	logPath = "./log.txt"
+
+	timestampFormat = "15:04:05.000"
 )
 
-var re = regexp.MustCompile(`^([^,]+),([^ ]+) --> ([^,]+),([^ ]+)$`)
+var (
+	re        = regexp.MustCompile(`^([^,]+),([^ ]+) --> ([^,]+),([^ ]+)$`)
+	logOutput = ""
+	// Threshold between dialog that will result in the silence being trimmed.
+	threshold, _ = time.ParseDuration("1s")
+)
+
+func print(message string) {
+	logOutput = logOutput + message
+	fmt.Println(message)
+}
+
+func newline() {
+	logOutput = logOutput + "\n"
+	fmt.Println()
+}
+
+func println(message string) {
+	print(message)
+	newline()
+}
+
+func printlnf(format string, a ...interface{}) {
+	println(fmt.Sprintf(format, a...))
+}
+
+func printf(format string, a ...interface{}) {
+	print(fmt.Sprintf(format, a...))
+}
 
 func runShellCommand(name string, arg ...string) ([]byte, error) {
 	cmd := exec.Command(name, arg...)
-	fmt.Println("Executing command: " + cmd.String())
+	println("Executing command: " + cmd.String())
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println(err.Error())
+		println(err.Error())
+		println("Command output: " + string(out))
 	}
 
-	fmt.Print("The output" + string(out))
 	return out, err
 }
 
-func main() {
-	if _, err := os.Stat(tempDir); os.IsNotExist(err) {
-		os.Mkdir(tempDir, 0755)
-	}
+// Interval represents a time interval over which subtitles are displayed.
+type Interval struct {
+	start string
+	end   string
+}
 
-	// First parameter
-	vidPath := os.Args[1]
-
-	_, err := runShellCommand("ffmpeg", "-i", vidPath, "-map", "0:s:0", tempDir+"subs.srt")
+func readAndCombineSubtitles(subPath string) []Interval {
+	file, err := os.Open(subPath)
 	if err != nil {
-		return
-	}
-
-	file, err := os.Open(tempDir + "subs.srt")
-	if err != nil {
+		println(err.Error())
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	outFile := ""
-
+	var readIn []Interval
 	scanner := bufio.NewScanner(file)
 	i := 0
 	for scanner.Scan() {
 		l := scanner.Text()
 		if strings.Contains(l, "-->") {
-			fname := "file-" + fmt.Sprint(i) + ".aac"
-			outFile = outFile + "file '" + fname + "'" + "\n"
 			start := re.ReplaceAllString(l, `$1.$2`)
-			stop := re.ReplaceAllString(l, `$3.$4`)
-			_, err = runShellCommand("ffmpeg", "-i", vidPath, "-ss", start, "-to", stop, "-c", "copy", tempDir+fname)
-			if err != nil {
-				return
-			}
+			end := re.ReplaceAllString(l, `$3.$4`)
+			readIn = append(readIn, Interval{start: start, end: end})
 			i = i + 1
 		}
 	}
@@ -70,15 +92,103 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = ioutil.WriteFile(tempDir+"output.txt", []byte(outFile), 0644)
+	return combineIntervals(readIn, threshold)
+}
+
+func gapOverThreshold(start, end string, gapThreshold time.Duration) bool {
+	startTime, err := time.Parse(timestampFormat, start)
 	if err != nil {
 		panic(err)
 	}
+	endTime, err := time.Parse(timestampFormat, end)
+	if err != nil {
+		panic(err)
+	}
+	if endTime.After(startTime) {
+		return endTime.Sub(startTime) > gapThreshold
+	}
+	return startTime.Sub(endTime) > gapThreshold
+}
 
-	_, err = runShellCommand("ffmpeg", "-f", "concat", "-safe", "0", "-i", tempDir+"output.txt", "-c", "copy", "combined.aac")
+func combineIntervals(intervals []Interval, gapThreshold time.Duration) []Interval {
+	if len(intervals) == 0 {
+		println("No subtitles found!")
+		log.Fatal("No subtitles found!")
+	}
+	// Sort.
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].start < intervals[j].start
+	})
+
+	var combined []Interval
+	pending := intervals[0]
+	printlnf("\n%s - %s  START\n", pending.start, pending.end)
+	for i := 1; i < len(intervals); i++ {
+		cur := intervals[i]
+		printf("%s - %s  ", cur.start, cur.end)
+		if cur.start < pending.end || !gapOverThreshold(pending.end, cur.start, gapThreshold) {
+			if cur.end >= pending.end {
+				print("overlap")
+				pending = Interval{start: pending.start, end: cur.end}
+			}
+		} else {
+			printf("              appended %s - %s  ", pending.start, pending.end)
+			if pending.start != pending.end {
+				combined = append(combined, pending)
+			}
+			pending = cur
+		}
+		newline()
+	}
+	if pending.start != pending.end {
+		combined = append(combined, pending)
+	}
+	return combined
+}
+
+func main() {
+	logOutput = ""
+	if _, err := os.Stat(tempDir); os.IsNotExist(err) {
+		os.Mkdir(tempDir, 0755)
+	}
+
+	// First parameter
+	vidPath := os.Args[1]
+
+	re2, _ := regexp.Compile(`.*/([^/]+).mkv`)
+	audioOutPath := re2.ReplaceAllString(vidPath, `$1.aac`)
+
+	_, err := runShellCommand("ffmpeg", "-y", "-i", vidPath, "-map", "0:s:0", tempDir+"subs.srt")
 	if err != nil {
 		return
 	}
 
+	comb := readAndCombineSubtitles(tempDir + "subs.srt")
+
+	outFile := ""
+	for i := 0; i < len(comb); i++ {
+		cur := comb[i]
+		fname := "file-" + fmt.Sprint(i) + ".aac"
+		outFile = outFile + "file '" + fname + "'" + "\n"
+		_, err = runShellCommand("ffmpeg", "-y", "-i", vidPath, "-ss", cur.start, "-to", cur.end, "-c", "copy", tempDir+fname)
+		if err != nil {
+			return
+		}
+	}
+
+	if err := ioutil.WriteFile(tempDir+"output.txt", []byte(outFile), 0644); err != nil {
+		panic(err)
+	}
+
+	if _, err = runShellCommand("ffmpeg", "-y", "-f", "concat", "-safe", "0", "-i", tempDir+"output.txt", "-c", "copy", audioOutPath); err != nil { //combined.aac
+		panic(err)
+	}
+
 	os.RemoveAll(tempDir)
+
+	if err := ioutil.WriteFile(logPath, []byte(logOutput), 0644); err != nil {
+		panic(err)
+	}
+
+	println("Action complete.")
 }
---
+++ ExtractDialog_test.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func duration(d string) time.Duration {
+	dur, err := time.ParseDuration(d)
+	if err != nil {
+		panic(err)
+	}
+	return dur
+}
+
+func Test_combineIntervals(t *testing.T) {
+	type args struct {
+		intervals []Interval
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Interval
+	}{
+		{
+			name: "NonOverlapping",
+			args: args{
+				intervals: []Interval{
+					Interval{start: "00:00:00.001", end: "00:00:00.005"},
+					Interval{start: "00:00:00.008", end: "00:00:00.009"},
+				},
+			},
+			want: []Interval{
+				Interval{start: "00:00:00.001", end: "00:00:00.005"},
+				Interval{start: "00:00:00.008", end: "00:00:00.009"},
+			},
+		},
+		{
+			name: "Backwards",
+			args: args{
+				intervals: []Interval{
+					Interval{start: "00:00:00.008", end: "00:00:00.009"},
+					Interval{start: "00:00:00.001", end: "00:00:00.005"},
+				},
+			},
+			want: []Interval{
+				Interval{start: "00:00:00.001", end: "00:00:00.005"},
+				Interval{start: "00:00:00.008", end: "00:00:00.009"},
+			},
+		},
+		{
+			name: "WithinThreshold",
+			args: args{
+				intervals: []Interval{
+					Interval{start: "00:00:00.001", end: "00:00:00.006"},
+					Interval{start: "00:00:00.008", end: "00:00:00.009"},
+				},
+			},
+			want: []Interval{
+				Interval{start: "00:00:00.001", end: "00:00:00.009"},
+			},
+		},
+		{
+			name: "Contained",
+			args: args{
+				intervals: []Interval{
+					Interval{start: "00:00:00.001", end: "00:00:00.005"},
+					Interval{start: "00:00:00.003", end: "00:00:00.004"},
+					Interval{start: "00:00:00.004", end: "00:00:00.005"},
+					Interval{start: "00:00:00.005", end: "00:00:00.009"},
+					Interval{start: "00:00:01.005", end: "00:00:03.009"},
+				},
+			},
+			want: []Interval{
+				Interval{start: "00:00:00.001", end: "00:00:00.009"},
+				Interval{start: "00:00:01.005", end: "00:00:03.009"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := combineIntervals(tt.args.intervals, duration("0.002s")); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("combineIntervals() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_gapOverThreshold(t *testing.T) {
+	type args struct {
+		start        string
+		end          string
+		gapThreshold time.Duration
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "WithinThreshold",
+			args: args{
+				start:        "00:00:00.001",
+				end:          "00:00:00.002",
+				gapThreshold: duration("0.002s"),
+			},
+			want: false,
+		},
+		{
+			name: "WithinThreshold_Backwards",
+			args: args{
+				start:        "00:00:00.002",
+				end:          "00:00:00.001",
+				gapThreshold: duration("0.002s"),
+			},
+			want: false,
+		},
+		{
+			name: "OverThreshold",
+			args: args{
+				start:        "00:00:00.001",
+				end:          "00:00:00.005",
+				gapThreshold: duration("0.002s"),
+			},
+			want: true,
+		},
+		{
+			name: "OverThreshold_Backwards",
+			args: args{
+				start:        "00:00:00.005",
+				end:          "00:00:00.001",
+				gapThreshold: duration("0.002s"),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := gapOverThreshold(tt.args.start, tt.args.end, tt.args.gapThreshold); got != tt.want {
+				t.Errorf("gapOverThreshold() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
