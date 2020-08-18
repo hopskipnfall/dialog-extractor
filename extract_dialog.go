f59ae5cc22d9328d9333063f9af849260e62b676
Jonn <hopskipnfall@users.noreply.github.com>
2020-08-19 02:35:48 +0900

    Let users choose audio/subtitle tracks if multiple exist (#1)
    Support for files with multiple audio/subtitle tracks
    Starting to refactor ffmpeg calls and logging
    Progress indicator
    
    Co-authored-by: jonn <jonnjonn@DESKTOP-DFCGIUM.localdomain>

--- extract_dialog.go
+++ extract_dialog.go
@@ -5,25 +5,31 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
+	"./ffmpeg"
 	"./logger"
+	"./shell"
+	"github.com/cheggaaa/pb/v3"
 )
 
 const (
 	tempDir   = "./.tmp/"
 	outputDir = "./out/"
 
-	timestampFormat = "15:04:05.000"
+	timestampFormat   = "15:04:05.000"
+	ffmpegInputNumber = 0
 )
 
 var (
 	srtTimingRegex = regexp.MustCompile(`^([^,]+),([^ ]+) --> ([^,]+),([^ ]+)$`)
 	videoPathRegex = regexp.MustCompile(`.*/([^/]+).mkv`)
+
+	progressBarTemplate = `{{string . "current_action" | green}} {{ bar . "[" "▮" (cycle . "▮" ) "_"}} {{percent .}}`
 
 	// Threshold for trimming a gap between dialog segments.
 	threshold, _ = time.ParseDuration("1.5s")
@@ -33,16 +39,10 @@ var (
 	l       = logger.New(&logPath)
 )
 
-func runShellCommand(name string, arg ...string) ([]byte, error) {
-	cmd := exec.Command(name, arg...)
-	l.Println("Executing command: " + cmd.String())
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		l.Println(err.Error())
-		l.Println("Command output: " + string(out))
-	}
-
-	return out, err
+// Configuration all configuration options chosen to extract dialog from a video.
+type Configuration struct {
+	Subtitles ffmpeg.Stream
+	Audio     ffmpeg.Stream
 }
 
 func main() {
@@ -57,36 +57,91 @@ func main() {
 	// Video path is the first argument.
 	vidPath := os.Args[1]
 
-	processFile(vidPath)
+	v := ffmpeg.NewVideo(l, vidPath)
+	err := v.LogFullFileInfo()
+	if err != nil {
+		l.Fatal(err.Error())
+	}
+
+	c := &Configuration{}
+
+	s, err := v.GetAudioStreams()
+	if err != nil {
+		l.Fatal(err.Error())
+	}
+
+	if len(s) == 0 {
+		l.Fatal("no audio tracks found")
+	} else if len(s) == 1 {
+		l.Printlnf("Found one audio track: %s (%s)", s[0].Tags.Title, s[0].Tags.Language)
+		c.Audio = s[0]
+	} else {
+		l.Println("Found multiple audio tracks:")
+		for i := 0; i < len(s); i++ {
+			cur := s[i]
+			l.Printlnf("\tOption %d: %s (%s)", i, cur.Tags.Title, cur.Tags.Language)
+		}
+		choice := requestInt("Choose option: ", 0, len(s)-1)
+		c.Audio = s[choice]
+	}
+
+	s, err = v.GetSubtitleStreams()
+	if err != nil {
+		l.Fatal(err.Error())
+	}
+
+	if len(s) == 0 {
+		l.Fatal("no subtitle tracks found")
+	} else if len(s) == 1 {
+		l.Printlnf("Found one subtitle track: %s (%s)", s[0].Tags.Title, s[0].Tags.Language)
+		c.Subtitles = s[0]
+	} else {
+		l.Println("Found multiple subtitle tracks:")
+		for i := 0; i < len(s); i++ {
+			cur := s[i]
+			l.Printlnf("\tOption %d: %s (%s)", i, cur.Tags.Title, cur.Tags.Language)
+		}
+		choice := requestInt("Choose option: ", 0, len(s)-1)
+		c.Subtitles = s[choice]
+	}
+
+	processFile(vidPath, *c)
 
 	// Write to log file.
 	l.WriteToFile()
-	l.Println("Action complete.")
 }
 
-func processFile(vidPath string) {
+func processFile(vidPath string, c Configuration) {
+
 	audioOutPath := videoPathRegex.ReplaceAllString(vidPath, `$1.mp3`)
 
-	// TODO: Do something better about selecting the subtitle track. Currently selects the first subtitle track.
-	_, err := runShellCommand("ffmpeg", "-y", "-i", vidPath, "-map", "0:s:0", tempDir+"subs.srt")
+	_, err := shell.ExecuteCommand(l, "ffmpeg", "-y", "-i", vidPath, "-map", fmt.Sprintf("%d:%d", ffmpegInputNumber, c.Subtitles.Index), tempDir+"subs.srt")
 	if err != nil {
 		return
 	}
 
 	comb := readAndCombineSubtitles(tempDir + "subs.srt")
 
+	// Create progress bar.
+	bar := pb.ProgressBarTemplate(progressBarTemplate).Start(len(comb) + 3)
+
 	mp3ScratchPath := tempDir + "full_audio.mp3"
-	// TODO: Do something better about selecting the audio track. Currently selects the first audio track.
-	_, err = runShellCommand("ffmpeg", "-y", "-i", vidPath, "-q:a", "0", "-map", "0:a:0", mp3ScratchPath)
+
+	bar.Set("current_action", "Copying audio")
+	_, err = shell.ExecuteCommand(l, "ffmpeg", "-y", "-i", vidPath, "-q:a", "0", "-map", fmt.Sprintf("%d:%d", ffmpegInputNumber, c.Audio.Index), mp3ScratchPath)
+	bar.Increment()
+
 	outFile := ""
 	for i := 0; i < len(comb); i++ {
 		cur := comb[i]
 		fname := "shard-" + fmt.Sprint(i) + ".mp3"
 		outFile = outFile + "file '" + fname + "'" + "\n"
-		_, err = runShellCommand("ffmpeg", "-y", "-i", mp3ScratchPath, "-ss", cur.start, "-to", cur.end, "-q:a", "0", "-map", "a", tempDir+fname)
+		bar.Set("current_action", fmt.Sprintf("Splitting audio (%d/%d)", i+1, len(comb)))
+		_, err = shell.ExecuteCommand(l, "ffmpeg", "-y", "-i", mp3ScratchPath, "-ss", cur.start, "-to", cur.end, "-q:a", "0", "-map", "a", tempDir+fname)
 		if err != nil {
 			return
 		}
+		bar.Increment()
 	}
 
 	// Write all fragment filenames to a text file.
@@ -95,17 +150,24 @@ func processFile(vidPath string) {
 	}
 
 	// Combine all fragments into one file.
-	if _, err = runShellCommand("ffmpeg", "-y", "-f", "concat", "-safe", "0", "-i", tempDir+"output.txt", "-c", "copy", tempDir+audioOutPath); err != nil {
+	bar.Set("current_action", "Joining audio fragments")
+	if _, err = shell.ExecuteCommand(l, "ffmpeg", "-y", "-f", "concat", "-safe", "0", "-i", tempDir+"output.txt", "-c", "copy", tempDir+audioOutPath); err != nil {
 		l.Fatal(err.Error())
 	}
+	bar.Increment()
 
 	// Re-encode output file to repair any errors from catenation.
-	if _, err = runShellCommand("ffmpeg", "-y", "-i", tempDir+audioOutPath, "-c:v", "copy", outputDir+audioOutPath); err != nil {
+	bar.Set("current_action", "Re-encoding audio")
+	if _, err = shell.ExecuteCommand(l, "ffmpeg", "-y", "-i", tempDir+audioOutPath, "-c:v", "copy", outputDir+audioOutPath); err != nil {
 		l.Fatal(err.Error())
 	}
+	bar.Increment()
+	bar.Finish()
 
 	// Delete temp dir.
 	os.RemoveAll(tempDir)
+
+	l.Printlnf("Action completed. Created file %s", tempDir+audioOutPath)
 }
 
 // Interval represents a time interval over which subtitles are displayed.
@@ -185,4 +247,21 @@ func combineIntervals(intervals []Interval, gapThreshold time.Duration) []Interv
 		combined = append(combined, pending)
 	}
 	return combined
+}
+
+// requestInt asks the user for a bounded number using stdio.
+func requestInt(message string, min, max int) int {
+	for true {
+		fmt.Print(message)
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+		choice, err := strconv.Atoi(text)
+		if err != nil || choice < min || choice > max {
+			l.Println("illegal choice, try again.")
+		} else {
+			return choice
+		}
+	}
+	panic("this is impossible")
 }
